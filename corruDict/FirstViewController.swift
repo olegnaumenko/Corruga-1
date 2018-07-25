3f6189565c009610d0619645f2603ca4201e8572
oleg.naumenko <oleg.naumenko@seniordevonly.com>
2018-07-25 13:31:38 +0300

    Initial Commit

---
+++ corruDict/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  corruDict
+//
+//  Created by oleg.naumenko on 7/25/18.
+//  Copyright © 2018 oleg.naumenko. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ corruDict/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  corruDict
+//
+//  Created by oleg.naumenko on 7/25/18.
+//  Copyright © 2018 oleg.naumenko. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
